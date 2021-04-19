936107de5c1b99e8a3d019fd30c959a306e662a6
Bux42 <virgile.desvaux@gmail.com>
2021-04-19 15:08:20 +0200

    node server

--- Server/app.js
+++ Server/app.js
@@ -11,25 +11,25 @@ app.use(cors({
     credentials: true
 }));
 
-const port = 3000;
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-});
-
 var db = null;
 const mongoClient = new MongoClient("mongodb://localhost:27017", {
     useUnifiedTopology: true
 });
 
 mongoClient.connect(function (err) {
-    console.log('MongoDB connected');
+    console.log('Connected successfully to server');
     db = mongoClient.db("dojo");
 });
 
 if (!fs.existsSync("maps")) {
     fs.mkdirSync("maps");
 }
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
 
 app.get("/auth", (req, res, next) => {
     console.log("/auth", req.query);
@@ -60,6 +60,18 @@ app.get("/auth", (req, res, next) => {
     res.send("1");
 });
 
+app.get("/set-current-map", (req, res, next) => {
+    console.log(req.query);
+    if (!fs.existsSync("maps/" + req.query.author)) {
+        fs.mkdirSync("maps/" + req.query.author);
+    }
+    if (!fs.existsSync("maps/" + req.query.author + "/" + req.query.name)) {
+        fs.mkdirSync("maps/" + req.query.author + "/" + req.query.name);
+    }
+    res.send("1");
+});
+
+
 app.post("/save-game-data", function (req, res) {
     console.log(req.query);
     var size = 0;
@@ -67,24 +79,16 @@ app.post("/save-game-data", function (req, res) {
     req.on('data', function (data) {
         size += data.length;
         data2 += data;
-        console.log('Got chunk: ' + data.length + ' total: ' + size);
     });
 
     req.on('end', function () {
         let buff = new Buffer(data2, 'base64');
-        console.log("total size = " + size);
         var filePath = "maps/" + req.query.authorName + "/" + req.query.mapName + "/" + req.query.endRaceTime + "_" + req.query.playerName + "_" + Date.now();
-        if (!fs.existsSync("maps/" + req.query.authorName)) {
-            fs.mkdirSync("maps/" + req.query.authorName);
-        }
-        if (!fs.existsSync("maps/" + req.query.authorName + "/" + req.query.mapName)) {
-            fs.mkdirSync("maps/" + req.query.authorName + "/" + req.query.mapName);
-        }
         fs.writeFile(filePath, buff, function (err) {
             if (err) {
                 return console.log(err);
             }
-            console.log("The file was saved!");
+            console.log("The file was saved at", filePath);
             data2 = "";
             const raceData = db.collection('race_data');
             req.query["file_path"] = filePath;
@@ -100,4 +104,35 @@ app.post("/save-game-data", function (req, res) {
         console.log("ERROR ERROR: " + e.message);
     });
 
+});
+
+// FRONT
+
+app.get("/get-files", (req, res, next) => {
+    console.log(req.query);
+    const raceData = db.collection('race_data');
+    var query = {};
+    if (req.query.mapName.length > 0) {
+        query["mapName"] = {$regex : ".*" + req.query.mapName + ".*"};
+    }
+    if (req.query.playerName.length > 0) {
+        query["playerName"] = {$regex : ".*" + req.query.playerName + ".*"};
+    }
+    if (req.query.raceFinished != -1) {
+        query["raceFinished"] = parseInt(req.query.raceFinished);
+    }
+    raceData.find(query).toArray(function (err, docs) {
+        var files = [];
+        console.log(docs);
+        for (var i = 0; i < req.query.maxResults && i < docs.length; i++) {
+            files.push(docs[i]);
+        }
+        res.send({"Files": files, "TotalResults": docs.length});
+    });
+   
+});
+
+app.get('/get-race-data', (req, res, nexct) => {
+    console.log(req.query);
+    res.sendFile(__dirname + "\\" + req.query.filePath);
 });
