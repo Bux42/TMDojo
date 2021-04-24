7b18e21dfb05bb264d1cc4564617971f8de34cfb
Bux42 <virgile.desvaux@gmail.com>
2021-04-24 22:17:11 +0200

    added map block extractor plugin & save system & started deserializing of map blocks in front

--- Server/app.js
+++ Server/app.js
@@ -25,6 +25,10 @@ if (!fs.existsSync("maps")) {
     fs.mkdirSync("maps");
 }
 
+if (!fs.existsSync("mapBlocks")) {
+    fs.mkdirSync("mapBlocks");
+}
+
 const port = 3000;
 
 app.listen(port, () => {
@@ -72,6 +76,32 @@ app.get("/set-current-map", (req, res, next) => {
     res.send("1");
 });
 
+app.post("/save-map-blocks", function (req, res) {
+    console.log(req.query);
+    var size = 0;
+    var data2 = "";
+    req.on('data', function (data) {
+        size += data.length;
+        data2 += data;
+    });
+
+    req.on('end', function () {
+        let buff = new Buffer(data2, 'base64');
+        var filePath = "mapBlocks/" + req.query.challengeId;
+        fs.writeFile(filePath, buff, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The file was saved at", filePath);
+            data2 = "";
+        });
+        res.end("Thanks");
+    });
+
+    req.on('error', function (e) {
+        console.log("ERROR ERROR: " + e.message);
+    });
+});
 
 app.post("/save-game-data", function (req, res) {
     console.log(req.query);
@@ -167,6 +197,11 @@ app.get("/get-files", (req, res, next) => {
 app.get('/get-race-data', (req, res, nexct) => {
     console.log(req.query); 
     res.sendFile(__dirname + "\\" + req.query.filePath);
+});
+
+app.get('/get-map-blocks', (req, res) => {
+    console.log(req.query); 
+    res.sendFile(__dirname + "\\mapBlocks\\" + req.query.filePath);
 });
 
 
