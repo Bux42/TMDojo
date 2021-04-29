7c7e2b3075f3339a1e0ac6b1893facf4dfcde91e
root <root@Debian-10.89.234.180.19>
2021-04-29 17:48:31 +0200

    check vercel env test number 2

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -8,6 +8,8 @@ interface Props {
 }
 
 const MyApp = ({ Component, pageProps }: Props): React.ReactElement => {
+	console.log("env:", process.env);
+	console.log("nodeApi:", process.env.NODE_API_VERCEL);
     return (
         <SettingsProvider>
             <Component {...pageProps} />
