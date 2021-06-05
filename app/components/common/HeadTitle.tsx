8c38ea4960ac15ba66005caff608606b50b0ed6d
Bux42 <virgile.desvaux@gmail.com>
2021-06-06 00:26:54 +0200

    add google analytics

--- app/components/common/HeadTitle.tsx
+++ app/components/common/HeadTitle.tsx
@@ -14,6 +14,18 @@ const HeadTitle = ({ mapInfo }: HeadTitleProps): JSX.Element => {
 
     return (
         <Head>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-0V30RMEPFN"></script>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                
+                    gtag('config', 'G-0V30RMEPFN');
+                `,
+                }}
+            />
             <title>{pageTitle}</title>
         </Head>
     );
