bd3c08e182fa206183fc312656eb0234a5b549ce
MKuijpers <mika.kuijpers@live.nl>
2021-11-29 22:16:01 +0100

    Cleanup bufferParser on server

--- server/src/webSocketServer/bufferParser.ts
+++ server/src/webSocketServer/bufferParser.ts
@@ -1,28 +1,10 @@
-// type Vector3 = [
-//     x: number,
-//     y: number,
-//     z: number,
-// ]
-
 interface LiveSample {
     raceTime: number;
     position: number[]
 }
 
 const parseBuffer = (buffer: Buffer): LiveSample | undefined => {
-    // let index = 0;
-    // for (let i = 0; index <= buffer.length - 4; i++) {
-    //     let val = 0;
-    //     if (index <= 1) {
-    //         val = buffer.readInt32LE(index);
-    //         index += 4;
-    //     } else {
-    //         val = buffer.readFloatLE(index);
-    //         index += 4;
-    //     }
-    //     console.log(val);
-    // }
-
+    // Check if buffer matches the desired length
     if (buffer.length === 16) {
         const raceTime = buffer.readInt32LE(0);
         const position = [buffer.readFloatLE(4), buffer.readFloatLE(8), buffer.readFloatLE(12)];
