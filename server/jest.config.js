8cf77c598759372ea792366f94008dc96d8da9b3
MKuijpers <mika.kuijpers@live.nl>
2022-04-07 15:51:09 +0200

    Add jest coverage npm script and coverage config

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    collectCoverageFrom: [
+        'src/**',
+    ],
+    coveragePathIgnorePatterns: [
+        // add patterns to ignore here
+    ],
 };
