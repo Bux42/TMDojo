01b109db62e9fa5de963c9d54fdf15698416be5b
MKuijpers <mika.kuijpers@live.nl>
2022-05-03 15:45:34 +0200

    Add generated Swagger API documentation for dev builds

--- server_combined/src/main.ts
+++ server_combined/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { config } from 'dotenv';
 import { AppModule } from './app.module';
+
+config();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    if (process.env.NODE_ENV === 'dev') {
+        const swaggerConfig = new DocumentBuilder()
+            .setTitle('TMDojo')
+            .setDescription('TMDojo API description')
+            .setVersion('1.0')
+            .build();
+        const document = SwaggerModule.createDocument(app, swaggerConfig);
+        SwaggerModule.setup('api', app, document);
+    }
+
     await app.listen(3000);
 }
 bootstrap();
