4744e29dc179f0a804de50f85079c32d66ae2ea8
MKuijpers <mika.kuijpers@live.nl>
2022-12-29 20:58:25 +0100

    Use 404 HttpError for /users errors

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -16,6 +16,7 @@ import { z } from 'zod';
 import * as db from '../lib/db';
 import zParseRequest from '../lib/zodParseRequest';
 import { wrap } from '../lib/asyncErrorHandler';
+import { HttpError } from '../lib/httpErrors';
 
 const router = express.Router();
 
@@ -33,11 +34,12 @@ router.get('/:webId/info', wrap(async (req: Request, res: Response) => {
     const { params: { webId } } = zParseRequest(userInfoInputSchema, req);
 
     const userInfos = await db.getUserByWebId(webId);
+
     if (!userInfos) {
         req.log.error(`usersRouter: User with webId "${webId}" not found`);
-        res.status(404).send();
-        return;
+        throw new HttpError(404, `User with webId '${webId}' not found`);
     }
+
     res.send(userInfos);
 }));
 
@@ -55,6 +57,12 @@ router.get('/:webId/replays', wrap(async (req: Request, res: Response) => {
     const { params: { webId } } = zParseRequest(userReplaysInputSchema, req);
 
     const userInfos = await db.getUserByWebId(webId);
+
+    if (!userInfos) {
+        req.log.error(`userReplaysRouter: User with webId "${webId}" not found`);
+        throw new HttpError(404, `User with webId '${webId}' not found`);
+    }
+
     const userReplays = await db.getReplaysByUserRef(userInfos._id);
     res.send(userReplays);
 }));
