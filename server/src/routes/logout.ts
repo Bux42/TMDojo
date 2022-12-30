70456130e36ba5be8315feb6dfd6ab49be3a11f5
MKuijpers <mika.kuijpers@live.nl>
2022-12-30 22:04:00 +0100

    Add input validation (more just type inference here) for logout route

--- server/src/routes/logout.ts
+++ server/src/routes/logout.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from 'express';
 import * as express from 'express';
+import { z } from 'zod';
 import { setExpiredSessionCookie } from '../lib/authorize';
 import { deleteSession, findSessionBySecret } from '../lib/db';
+import zParseRequest from '../lib/zodParseRequest';
+import { asyncErrorHandler } from '../lib/asyncErrorHandler';
+import { HttpError } from '../lib/httpErrors';
 
 const router = express.Router();
 
@@ -9,38 +13,45 @@ const router = express.Router();
  * POST /logout
  * Logs user out be deleting their session
  */
-router.post('/', async (req: Request, res: Response, next: Function) => {
-    try {
-        let { sessionId } = req.cookies;
-
-        // Check if request originated from plugin
-        if (!sessionId) {
-            sessionId = req.body.sessionId;
-        }
-
-        // Check for missing parameters
-        if (sessionId === undefined || typeof sessionId !== 'string') {
-            res.status(401).send({ message: 'No session secret supplied.' });
-            return;
-        }
-
-        // Check if the session exists
-        const session = await findSessionBySecret(sessionId);
-        if (session === null || session === undefined) {
-            req.log.warn('logoutRouter: Session not found, deleting session cookie anyway');
-            setExpiredSessionCookie(req, res);
-            res.status(401).send({ message: 'No session found for this secret.' });
-            return;
-        }
-
-        req.log.debug('logoutRouter: Deleting session and session cookie');
-        await deleteSession(sessionId);
-        setExpiredSessionCookie(req, res);
-
-        res.status(200).end();
-    } catch (err) {
-        next(err);
-    }
+const logoutInputSchema = z.object({
+    body: z.object({
+        sessionId: z.string().optional(),
+    }),
+    cookies: z.object({
+        sessionId: z.string().optional(),
+    }),
 });
+
+router.post('/', asyncErrorHandler(async (req: Request, res: Response) => {
+    const { body, cookies } = zParseRequest(logoutInputSchema, req);
+
+    // Get sessionId from cookie or body
+    let sessionId;
+    if (cookies.sessionId) {
+        req.log.debug('logoutRouter: Using sessionId from cookies');
+        sessionId = cookies.sessionId;
+    } else if (body.sessionId) {
+        req.log.debug('logoutRouter: Using sessionId from body');
+        sessionId = body.sessionId;
+    } else {
+        req.log.warn('logoutRouter: Logout attempt without a sessionId');
+        throw new HttpError(401, 'No sessionId provided.');
+    }
+
+    // Check if the session exists
+    const session = await findSessionBySecret(sessionId);
+    if (!session) {
+        req.log.warn('logoutRouter: Session not found, deleting session cookie anyway');
+        setExpiredSessionCookie(req, res);
+        throw new HttpError(401, 'Session not found.');
+    }
+
+    // Delete session and set session cookie to instantly expire
+    req.log.debug('logoutRouter: Deleting session and session cookie');
+    await deleteSession(sessionId);
+    setExpiredSessionCookie(req, res);
+
+    res.status(200).end();
+}));
 
 export default router;
