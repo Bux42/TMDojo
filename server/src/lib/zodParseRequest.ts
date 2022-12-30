6c3b11cbd6dcf20ea2866f9518de5f7b35ac4b04
MKuijpers <mika.kuijpers@live.nl>
2022-12-30 20:17:45 +0100

    Format path of issues in zod parsing errors

--- server/src/lib/zodParseRequest.ts
+++ server/src/lib/zodParseRequest.ts
@@ -2,6 +2,26 @@ import type { Request } from 'express';
 import { AnyZodObject, z } from 'zod';
 import { HttpError } from './httpErrors';
 
+/**
+ * Formats a path array of strings and number indices into a string
+ * Normal strings are separated by dots, number indices are formatted with brackets around to indicate array indices
+ */
+const formatPath = (path: (string | number)[]) => {
+    if (path.length === 0) return '';
+
+    let res = `${path[0]}`;
+
+    for (let i = 1; i < path.length; i++) {
+        if (typeof path[i] === 'number') {
+            res += `[${path[i]}]`;
+        } else {
+            res += `.${path[i]}`;
+        }
+    }
+
+    return res;
+};
+
 const zParseRequest = <T extends AnyZodObject>(
     schema: T,
     req: Request,
@@ -14,7 +34,7 @@ const zParseRequest = <T extends AnyZodObject>(
             req.log.error(`zParseRequest: ZodError during parsing: ${JSON.stringify(err.issues)}`);
 
             const readableIssues = err.issues.map((issue) => ({
-                path: issue.path,
+                path: formatPath(issue.path),
                 message: issue.message,
             }));
 
