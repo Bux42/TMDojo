4a01e03849023042ec8b851cae0bc80e58887de1
MKuijpers <mika.kuijpers@live.nl>
2023-07-13 21:46:38 +0200

    Remove IsNotEmpty for mapUId and mapName on /maps endpoint

--- server_combined/src/maps/dto/list-maps.dto.ts
+++ server_combined/src/maps/dto/list-maps.dto.ts
@@ -1,12 +1,10 @@
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import { IsOptional } from 'class-validator';
 import { PaginationDto } from '../../common/dto/pagination.dto';
 
 export class ListMapsDto extends PaginationDto {
     @IsOptional()
-    @IsNotEmpty()
     mapUId?: string;
 
     @IsOptional()
-    @IsNotEmpty()
     mapName?: string;
 }
