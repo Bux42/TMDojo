6a521e9f2749d2f16fd3e91af7023bcb6b969783
MKuijpers <mika.kuijpers@live.nl>
2023-07-13 21:49:56 +0200

    Reject negative pagination values for limit, skip, and skipPage

--- server_combined/src/common/dto/pagination.dto.ts
+++ server_combined/src/common/dto/pagination.dto.ts
@@ -1,5 +1,7 @@
 import { Type } from 'class-transformer';
-import { IsInt, IsNotEmpty, IsOptional } from 'class-validator';
+import {
+    IsInt, IsNotEmpty, IsOptional, Min,
+} from 'class-validator';
 
 // type SortAsc = 'asc' | 'ASC' | 1
 // type SortDesc = 'desc' | 'DESC' | -1
@@ -8,18 +10,21 @@ export class PaginationDto {
     @IsOptional()
     @IsNotEmpty()
     @IsInt()
+    @Min(0)
     @Type(() => Number)
     limit?: number;
 
     @IsOptional()
     @IsNotEmpty()
     @IsInt()
+    @Min(0)
     @Type(() => Number)
     skip?: number;
 
     @IsOptional()
     @IsNotEmpty()
     @IsInt()
+    @Min(0)
     @Type(() => Number)
     skipPage?: number;
 
